db5e988661b88e6e6fd4f6a00a1deedf0ef9c176
Leo Joseph <rleo2joseph@gmail.com>
2015-03-27 18:49:43 +0530

    .
    .

--- src/ios/Beacon.m
+++ src/ios/Beacon.m
@@ -33,7 +33,7 @@
     [self registerNotifications];
     [self writeErrorLog:@"\n 2"];
 
-    [Gimbal setAPIKey:@"d1c5ea32-a1ee-405b-9bd8-88255ea574cc" options:nil];
+    //[Gimbal setAPIKey:@"d1c5ea32-a1ee-405b-9bd8-88255ea574cc" options:nil];
     [self writeErrorLog:@"\n 3"];
 
     self.beaconManager = [GMBLBeaconManager new];
