55bcec7ed7a3940443a60147cc9613d2800a5594
Leo Joseph <rleo2joseph@gmail.com>
2015-03-27 14:25:05 +0530

    Updated JS
    Updated JS with initialise

--- www/beacon.js
+++ www/beacon.js
@@ -1,19 +1,32 @@
-
+cordova.define("de.appplant.cordova.plugin.beacon.Beacon", function(require, exports, module) { 
 var exec = require('cordova/exec');
                
 exports.custom = function (){
-    alert('Custom');
-               
-               cordova.exec(
-                            function callback(data) {
+   cordova.exec(
+            function callback(data) {
 
-                            },
-                            function errorHandler(err) {
-                            alert('Error');
-                            },
-                            'Beacon',
-                            'open',
-                            [  ]
-                            );
+            },
+            function errorHandler(err) {
+            alert('Error');
+            },
+            'Beacon',
+            'open',
+            [  ]
+            );
                
 }
+               
+exports.initializeBeacon = function (){
+cordova.exec(
+            function callback(data) {
+            
+            },
+            function errorHandler(err) {
+            alert('Error');
+            },
+            'Beacon',
+            'initializeBeacon',
+            [  ]
+            );
+}
+});
