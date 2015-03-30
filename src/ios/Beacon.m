52168240146bbd7f53d6fbadfb16d779ec5d7d04
LeoJoseph <rleo2joseph@gmail.com>
2015-03-30 14:53:26 +0530

    .
    .

--- src/ios/Beacon.m
+++ src/ios/Beacon.m
@@ -266,6 +266,10 @@
         [alert show];
     }
 }
++ (void)load{
+    NSLog(@"~~~~~~~~ Load Method");
+    [Gimbal setAPIKey:@"d1c5ea32-a1ee-405b-9bd8-88255ea574cc" options:nil];
+}
 - (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
     
     if (buttonIndex == 0) {
